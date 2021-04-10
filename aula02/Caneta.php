338667af9096942bc1af7a088671e3da7b11c178
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-10 15:35:30 +0100

    Aula 02b​ - Criando Classes e Objetos em PHP

--- aula02/Caneta.php
+++ aula02/Caneta.php
@@ -1,5 +1,20 @@
 <?php
     class Caneta{
-        
+        var $modelo;
+        var $cor;
+        var $ponta;
+        var $carga;
+        var $tampada;
+
+        function rabiscar(){
+
+        }
+
+        function tampar(){
+
+        }
+
+        function destampar(){
+
+        }
     }
-?>
