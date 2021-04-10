b720c5cb674b40c2598ab8904a7243796ebf4d9b
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-10 15:29:45 +0100

    Aula 02b​ - Criando Classes e Objetos em PHP

--- aula02/aula02b.php
+++ aula02/aula02b.php
@@ -1,4 +1,14 @@
-<?php
-    
-
-?>
+<!DOCTYPE html>
+<html lang="pt-Pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 02b​ - Criando Classes e Objetos em PHP</title>
+</head>
+<body>
+    <?php
+        
+    ?>
+</body>
+</html>
