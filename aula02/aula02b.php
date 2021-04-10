ad4119b5100c328b2035e4a45429edc453959c56
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-10 15:56:58 +0100

    Aula 02b​ - Criando Classes e Objetos em PHP

--- aula02/aula02b.php
+++ aula02/aula02b.php
@@ -10,14 +10,19 @@
     <?php
         require_once "Caneta.php";
 
-        $c1 = new Caneta();
-
+        $c1 = new Caneta;
         $c1->cor = "Azul";
         $c1->ponta = 0.5;
         $c1->tampada = false;
         $c1->destampar();
         $c1->rabiscar();
         var_dump($c1);
+
+        $c2 = new Caneta;
+        $c2->cor = "Verde";
+        $c2->carga = 50;
+        $c2->tampar();
+        
     ?>
 </body>
 </html>
