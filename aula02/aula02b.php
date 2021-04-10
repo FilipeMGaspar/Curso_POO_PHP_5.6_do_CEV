fdd837f4a7729f79cac0ea31c61bc307405e8522
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-10 15:53:42 +0100

    Aula 02b​ - Criando Classes e Objetos em PHP

--- aula02/aula02b.php
+++ aula02/aula02b.php
@@ -15,7 +15,7 @@
         $c1->cor = "Azul";
         $c1->ponta = 0.5;
         $c1->tampada = false;
-        $c1->tampar();
+        $c1->destampar();
 
         $c1->rabiscar();
         //var_dump($c1);
