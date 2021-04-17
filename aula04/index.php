6ef395cca83bc5fe806652f9974a99056e48f804
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-17 13:13:55 +0100

    Aula 04b​ - Métodos Getter, Setter e Construtor

--- aula04/index.php
+++ aula04/index.php
@@ -12,14 +12,7 @@
         <?php
             require_once "Caneta.php";
             $c1 = new Caneta();
-            $c1->setMarca("Uniball");
-            $c1->setCor("Preta");
-            $c1->setPonta(0.5);
-            $c1->setCarga(85);
-            $c1->tampar();
-            echo "<br>";
-            echo "Eu tenho uma caneta ". $c1->getMarca() ." de ponta ". $c1->getPonta();
-            echo "<br><br>";
+            
             print_r($c1);
 
         ?>
