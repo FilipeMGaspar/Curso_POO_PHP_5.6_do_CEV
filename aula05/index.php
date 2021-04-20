af824e0a2702403e55f52e9dffd6865d687e0d1b
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-20 11:03:45 +0100

    Aula 05b​ - Exemplo Prático em PHP

--- aula05/index.php
+++ aula05/index.php
@@ -12,13 +12,13 @@
             require_once "ContaBanco.php";
 
             $tst = new ContaBanco();
-            $tst->abrirConta("CC");
-           
+            //$tst->abrirConta("CC");
+            $tst ->depositar(20);
             print_r($tst);
 
             //$tst->setSaldoConta(0);
             //$tst->fecharConta();
-            
+
             // $tst->setNumConta(256);
             // $tst->setTipoConta("CC");
             // $tst->setDonoConta("Marineuza");
