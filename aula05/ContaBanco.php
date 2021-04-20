7bd5fbb874a8df27f8717f568c320005d30edd2b
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-20 10:14:18 +0100

    Aula 05b​ - Exemplo Prático em PHP

--- aula05/ContaBanco.php
+++ aula05/ContaBanco.php
@@ -6,7 +6,13 @@
         private $donoConta;
         private $saldoConta;
         private $statusConta; //Verdadeiro(true) conta aberta falso(false) conta fechada
-  
+        
+        //Métodos especiais
+        //Método Construtor
+        public function ContaBanco(){
+            $this->statusConta = false; //Conta fechada
+            $this->saldoConta = 0;
+        }
 
     }
 ?>
