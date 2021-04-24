2f0149437e183159f8cc24a3da644eedcfc15c5f
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-24 20:00:18 +0100

    Aula 06b​ - Encapsulamento

--- aula06/ControleRemoto.php
+++ aula06/ControleRemoto.php
@@ -13,7 +13,27 @@
             $this->tocando = false;
         }    
 
-        
+        public function getVolume(){
+            return $this->volume;
+        }
+        public function setVolume($vol){
+            $this->volume = $vol;
+        }   
+
+        public function getLigado(){
+            return $this->ligado;
+        }
+        public function setLigado($power){
+            $this->ligado = $power;
+        }
+
+        public function getTocando(){
+            return $this->tocando;
+        }
+        public function setTocando($reproducao){
+            $this->tocando = $reproducao;
+        }
+
 
     }
 ?>
