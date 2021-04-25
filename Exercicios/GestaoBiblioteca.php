e338a5781a358f10766bc58c14e86ef85c804a85
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-25 20:46:29 +0100

    Exercício 01 POO PHP 5.6

--- Exercicios/GestaoBiblioteca.php
+++ Exercicios/GestaoBiblioteca.php
@@ -1,16 +1,11 @@
 <?php
+require_once 'Biblioteca.php';
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of GestaoBiblioteca
- *
- * @author Aprender
- */
 class GestaoBiblioteca {
-    //put your code here
+    private $nomeAluno;
+    private $nomeAutor;
+    private $numemprestimo;
+    private $emprestado;
+    
+    
 }
