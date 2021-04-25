e29cb5543417a6965100f6724643184a7860f3e8
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-25 20:42:29 +0100

    Exercício 01 POO PHP 5.6

---
+++ Exercicios/Biblioteca.php
@@ -0,0 +1,8 @@
+<?php
+
+interface Biblioteca {
+    public function registarLivro();
+    public function registarAluno();
+    public function emprestarLivro();
+    public function reberLivroEmprestado();
+}
