ef0f74ad16f7ea3df82f0625e06a692b6ad412c3
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-25 20:52:00 +0100

    Exercício 01 POO PHP 5.6

--- Exercicios/GestaoBiblioteca.php
+++ Exercicios/GestaoBiblioteca.php
@@ -7,5 +7,38 @@ class GestaoBiblioteca {
     private $numemprestimo;
     private $emprestado;
     
+    public function __construct() {
+        $this->emprestado = false;
+    }
+    
+    public function getNomeAluno() {
+        return $this->nomeAluno;
+    }
+    public function setNomeAluno($nomeAluno) {
+        $this->nomeAluno = $nomeAluno;
+    }
+
+    public function getNomeAutor() {
+        return $this->nomeAutor;
+    }
+    public function setNomeAutor($nomeAutor) {
+        $this->nomeAutor = $nomeAutor;
+    }
+
+    public function getNumemprestimo() {
+        return $this->numemprestimo;
+    }
+     public function setNumemprestimo($numemprestimo) {
+        $this->numemprestimo = $numemprestimo;
+    }
+    
+    public function getEmprestado() {
+        return $this->emprestado;
+    }
+    public function setEmprestado($emprestado) {
+        $this->emprestado = $emprestado;
+    }
+    
+    //Métodos abstractos
     
 }
