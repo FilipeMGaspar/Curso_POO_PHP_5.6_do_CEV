ccb43a501d410b9ee8cc847c26cde1073a774043
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-25 21:05:35 +0100

    Exercício 01 POO PHP 5.6

--- Exercicios/index.php
+++ Exercicios/index.php
@@ -5,10 +5,14 @@
         <title></title>
     </head>
     <body>
+        <pre>
         <?php
             require_once 'Gestaobiblioteca.php';
             $eb = new GestaoBiblioteca();
+            $eb->emprestarLivro();
             
+            print_r($eb);
         ?>
+        </pre>
     </body>
 </html>
