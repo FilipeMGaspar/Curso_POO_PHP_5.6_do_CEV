475424249ebb6ebc018c1a8948ed8c741587032e
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-25 19:53:59 +0100

    Aula 06b​ - Encapsulamento

--- aula06/Controlador.php
+++ aula06/Controlador.php
@@ -1,7 +1,19 @@
 <?php
     interface Controlador {
         public function ligar();
-        /*public abstract function ligar();
+        public function desligar();
+        public function abrirMenu();
+        public function fecharMenu();
+        public function maisVolume();
+        public function menosVolume();
+        public function ligarMudo();
+        public function desligarMudo();
+        public function play();
+        public function pause();
+
+
+        /*
+        public abstract function ligar();
         public abstract function desligar();
         public abstract function abriMenu();
         public abstract function fecharMenu();
