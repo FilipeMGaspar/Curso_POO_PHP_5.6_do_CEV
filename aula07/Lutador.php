ee721a3d916046ac9c99c88f5fcd6ff2c0449534
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-03 20:03:54 +0100

    Aula 07b​ - Objetos Compostos em PHP

--- aula07/Lutador.php
+++ aula07/Lutador.php
@@ -10,6 +10,19 @@ class Lutador {
     private $vitorias;
     private $derrotas;
     private $empates;
+    
+    //Métodos especiais
+    public function __construct($nome, $nacionalidade, $idade, $altura, $peso, $vitorias, $derrotas, $empates) {
+        $this->nome = $nome;
+        $this->nacionalidade = $nacionalidade;
+        $this->idade = $idade;
+        $this->altura = $altura;
+        $this->peso = $peso;
+        $this->vitorias = $vitorias;
+        $this->derrotas = $derrotas;
+        $this->empates = $empates;
+    }
+
 }
 /*
    
