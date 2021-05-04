cdd4b83cc9c6b00dc35d82e1c0a3537a863c95d5
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-04 19:12:15 +0100

    Aula 07b​ - Objetos Compostos em PHP

--- aula07/index.php
+++ aula07/index.php
@@ -17,7 +17,8 @@
                 $l = array();
                 
                 $l[0] = new Lutador("Pretty Boy", "França", 31, 1.75, 68.9, 11, 2, 1);
-                $l[0]->apresentar();
+                //$l[0]->apresentar();
+                $l[0]->status();
                 
                 $l[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
                 echo '<br>';
@@ -25,7 +26,8 @@
                 
                 $l[2] = new Lutador("Snapshadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
                 echo '<br>';
-                $l[2]->apresentar();
+                //$l[2]->apresentar();
+                $l[2]->status();
                 
                 $l[3] = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
                 echo '<br>';
@@ -34,9 +36,13 @@
                 
                 $l[4] = new Lutador("UfoCobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
                 echo '<br>';
-                $l[4]->apresentar();
+                //$l[4]->apresentar();
+                $l[4]->status();
                
-                
+                $l[5] = new Lutador("Nerdaard", "EUA", 30, 1.81, 105.7, 12, 2, 4);
+                echo '<br>';
+                //$l[5]->apresentar();
+                $l[5]->status();
             ?>
         </pre>    
     </body>
