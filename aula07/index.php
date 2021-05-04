be36888bd65ad71a22606976dbaf03e684669778
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-04 18:54:20 +0100

    Aula 07b​ - Objetos Compostos em PHP

--- aula07/index.php
+++ aula07/index.php
@@ -8,11 +8,19 @@
         <pre>
             <?php
                 require_once 'Lutador.php';
-                $l = new Lutador("MegaKiller", "Tuga", 900, 1.85, 66.3, 200, 20, 85);
+                /*$l = new Lutador("MegaKiller", "Tuga", 900, 1.85, 66.3, 200, 20, 85);
                 $l->ganharLuta();
                 //print_r($l);
                 //$l->apresentar();
-                $l->status();
+                $l->status();*/
+                
+                $l = array();
+                
+                $l[0] = new Lutador("Pretty Boy", "França", 31, 1.75, 68.9, 11, 2, 1);
+                $l[0]->apresentar();
+                
+                $l[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+                $l[1]->status();
             ?>
         </pre>    
     </body>
