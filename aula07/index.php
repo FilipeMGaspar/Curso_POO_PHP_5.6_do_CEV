23010cad3f14472d6ae3f249fb4339343dd32021
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-04 18:23:39 +0100

    Aula 07b​ - Objetos Compostos em PHP

--- aula07/index.php
+++ aula07/index.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <pre>
+            <?php
+                require_once 'Lutador.php';
+                $l = new Lutador("Teste", "Tuga", 900, 1.85, 56.3, 200, 20, 85);
+                print_r($l);
+            ?>
+        </pre>    
     </body>
 </html>
