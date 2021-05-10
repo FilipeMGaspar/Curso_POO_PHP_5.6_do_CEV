958baaee52fa988ed7698100f06e1058735279f6
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-10 13:50:52 +0100

    Aula 08b​ - Agregação entre Objetos com PHP

--- aula08/Luta.php
+++ aula08/Luta.php
@@ -16,4 +16,34 @@ class Luta {
     public function lutar(){
         
     }
+    
+    //Métodos Getters e Setters
+    public function getDesafiado() {
+        return $this->desafiado;
+    }
+    public function setDesafiado($desafiado) {
+        $this->desafiado = $desafiado;
+    }
+
+    public function getDesafiante() {
+        return $this->desafiante;
+    }
+    public function setDesafiante($desafiante) {
+        $this->desafiante = $desafiante;
+    }
+
+    public function getRounds() {
+        return $this->rounds;
+    }
+    public function setRounds($rounds) {
+        $this->rounds = $rounds;
+    }
+
+    public function getAprovada() {
+        return $this->aprovada;
+    }
+    public function setAprovada($aprovada) {
+        $this->aprovada = $aprovada;
+    }
+
 }
