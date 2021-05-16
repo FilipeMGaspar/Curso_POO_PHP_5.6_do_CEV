a10ffe9fa78a896875e73ba6b4d6ce108f1dc869
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-16 19:35:01 +0100

    Aula 09b - Exercício prático POO em PHP

--- aula09/Pessoa.php
+++ aula09/Pessoa.php
@@ -13,5 +13,29 @@ class Pessoa {
     }
 
     //Métodos geters e seters
-    
+    public function getNome() {
+        return $this->nome;
+    }
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getIdade() {
+        return $this->idade;
+    }
+    public function setIdade($idade) {
+        $this->idade = $idade;
+    }
+
+    public function getSexo() {
+        return $this->sexo;
+    }
+    public function setSexo($sexo) {
+        $this->sexo = $sexo;
+    }
+
+    //Métodos públicos
+    public function fazerAniver(){
+        $this->setIdade($this->getIdade() + 1);
+    }
 }
