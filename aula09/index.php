480966c61e765cacd1a153e4d2e21d96b753b27f
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-16 19:36:46 +0100

    Aula 09b - Exercício prático POO em PHP

--- aula09/index.php
+++ aula09/index.php
@@ -5,8 +5,14 @@
         <title>Aula 09b - Exercício prático POO em PHP</title>
     </head>
     <body>
+        <pre>
         <?php
-        
+            require_once 'Pessoa.php'; 
+            
+            $ana = new Pessoa("ana", 16, 'F');
+            echo "<br>";
+            print_r($ana);
         ?>
+        </pre>
     </body>
 </html>
