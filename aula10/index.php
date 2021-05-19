c7fc973f2322aa80341f57c2b3881ca4f177825f
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-19 19:17:40 +0100

    Aula 10b - Herança (Parte 1) - PHP

--- aula10/index.php
+++ aula10/index.php
@@ -1,17 +1,13 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Aula 10b - Herança (Parte 1) - PHP</title>
     </head>
     <body>
         <?php
-        // put your code here
+        
         ?>
     </body>
 </html>
