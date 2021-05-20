33903caadbaa22c4ea445f7f8798ebc02bb80c53
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-20 19:40:22 +0100

    Aula 10b - Herança (Parte 1) - PHP

--- aula10/Aluno.php
+++ aula10/Aluno.php
@@ -3,4 +3,21 @@ class Aluno {
     //Atributos
     private $matr;
     private $curso;
+    
+    //Métodos Geters e Seters
+    public function getMatr() {
+        return $this->matr;
+    }
+    public function setMatr($matr) {
+        $this->matr = $matr;
+    }
+    
+    public function getCurso() {
+        return $this->curso;
+    }
+    public function setCurso($curso) {
+        $this->curso = $curso;
+    }
+
+
 }
