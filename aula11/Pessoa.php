2ed6bc48c22b073a79ac3654128d2235d92f38c4
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-25 12:50:44 +0100

    Aula 11b - Herança (Parte 2) - PHP

--- aula11/Pessoa.php
+++ aula11/Pessoa.php
@@ -6,4 +6,27 @@ abstract class Pessoa {
     private $idade;
     private $sexo;
     
+    //Métodos Geter e Seter
+    public function getNome() {
+        return $this->nome;
+    }
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+    
+    public function getIdade() {
+        return $this->idade;
+    }
+    public function setIdade($idade) {
+        $this->idade = $idade;
+    }
+    
+    public function getSexo() {
+        return $this->sexo;
+    }
+    public function setSexo($sexo) {
+        $this->sexo = $sexo;
+    }
+
+
 }
