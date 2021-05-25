ce4fa3c24ad51ecd2f992cb28ef06a7bc809144c
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-25 13:02:53 +0100

    Aula 11b - Herança (Parte 2) - PHP

---
+++ aula11/Aluno.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Aluno
+ *
+ * @author Aprender
+ */
+class Aluno {
+    //put your code here
+}
