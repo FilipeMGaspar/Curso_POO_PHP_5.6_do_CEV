e6245fd26998e5bdfec67ee8a3088cf9943aeff6
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-25 13:11:40 +0100

    Aula 11b - Herança (Parte 2) - PHP

--- aula11/index.php
+++ aula11/index.php
@@ -11,12 +11,21 @@
             //$p1 = new Pessoa(); com erro classe abestrata não pode ser instanciada
             
             require_once 'Visitante.php';
+            require_once 'Aluno.php';
             
             $v1 = new Visitante();
-            $v1->setNome("Juvenal");
-            $v1->setIdade(33);
-            $v1->setSexo('M');
-            print_r($v1);
+            //$v1->setNome("Juvenal");
+            //$v1->setIdade(33);
+            //$v1->setSexo('M');
+            //print_r($v1);
+            
+            $a1 = new Aluno();
+            $a1->setNome("Juliana");
+            $a1->setIdade(16);
+            $a1->setSexo('F');
+            $a1->setCurso("Enfermagem");
+            $a1->setMatricula(2021256123);
+            print_r($a1);
         ?>
         </pre>
     </body>
