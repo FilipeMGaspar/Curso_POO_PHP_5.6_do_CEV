fe6a7f3c0daf8ae2e28887611d9ca5435784cdd3
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-25 13:03:25 +0100

    Aula 11b - Herança (Parte 2) - PHP

--- aula11/Aluno.php
+++ aula11/Aluno.php
@@ -1,16 +1,10 @@
 <?php
+require_once 'Pessoa.php';
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Aluno
- *
- * @author Aprender
- */
-class Aluno {
-    //put your code here
+class Aluno extends Pessoa {
+   //Atributos
+    private $matricula;
+    private $curso;
+    
+    
 }
