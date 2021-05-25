bdc09a5cd45493c53ce943d92abcb810bdce1be4
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-25 13:07:15 +0100

    Aula 11b - Herança (Parte 2) - PHP

--- aula11/Aluno.php
+++ aula11/Aluno.php
@@ -6,5 +6,23 @@ class Aluno extends Pessoa {
     private $matricula;
     private $curso;
     
+    //Métodos Geter e Seter
+    public function getMatricula() {
+        return $this->matricula;
+    }
+    public function setMatricula($matricula) {
+        $this->matricula = $matricula;
+    }
     
+    public function getCurso() {
+        return $this->curso;
+    }
+    public function setCurso($curso) {
+        $this->curso = $curso;
+    }
+
+    //Métodos públicos
+    public function pagarMensalidade() {
+        echo "<p>O aluno <strong>".$this->getNome()."</strong> pagou a mensalide!</p>";
+    }
 }
