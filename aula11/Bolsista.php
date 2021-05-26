5db98b18b7e6acc80fda6edc787a51ac563c5165
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-26 13:33:30 +0100

    Aula 11b - Herança (Parte 2) - PHP

--- aula11/Bolsista.php
+++ aula11/Bolsista.php
@@ -2,9 +2,22 @@
 require_once 'Aluno.php';
 
 class Bolsista extends Aluno {
+    //Atributos
     private $bolsa;
     
+    //Método público
     private function renovarBolsa() {
         echo "<p>Bolsa Renovada</p>";
     }
+    
+    //Métodos Geter e Seter
+    public function getBolsa() {
+        return $this->bolsa;
+    }
+    public function setBolsa($bolsa) {
+        $this->bolsa = $bolsa;
+    }
+
+    
+
 }
