36028157aca9558e81fbcd8ddb347a168a50471b
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-26 13:28:43 +0100

    Aula 11b - Herança (Parte 2) - PHP

--- aula11/index.php
+++ aula11/index.php
@@ -12,6 +12,7 @@
             
             require_once 'Visitante.php';
             require_once 'Aluno.php';
+            require_once 'Bolsista.php';
             
             $v1 = new Visitante();
             //$v1->setNome("Juvenal");
@@ -20,13 +21,17 @@
             //print_r($v1);
             
             $a1 = new Aluno();
-            $a1->setNome("Juliana");
-            $a1->setIdade(16);
-            $a1->setSexo('F');
-            $a1->setCurso("Enfermagem");
-            $a1->setMatricula(2021256123);
-            print_r($a1);
-            $a1->pagarMensalidade();
+            //$a1->setNome("Juliana");
+            //$a1->setIdade(16);
+            //$a1->setSexo('F');
+            //$a1->setCurso("Enfermagem");
+            //$a1->setMatricula(2021256123);
+            //print_r($a1);
+            //$a1->pagarMensalidade();
+            
+            $b1 = new Bolsista();
+            $b1->setMatricula(2563265);
+            
         ?>
         </pre>
     </body>
