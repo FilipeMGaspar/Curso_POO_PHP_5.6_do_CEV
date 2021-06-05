62e771fb0ca73ebf0781e85f4ec354718cf48c4f
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-05 12:57:01 +0100

    Aula 12b - Polimorfismo em PHP (Parte 1)

--- aula12/index.php
+++ aula12/index.php
@@ -7,23 +7,35 @@
     </head>
     <body>
         <pre>       
- <?php
-        //require_once 'Animal.php';    
-        //$a = new Animal(); Uma classe abstrata não pode ser instanciada
- 
-        require_once 'Mamifero.php';
+        <?php
+            //require_once 'Animal.php';    
+            //$a = new Animal(); Uma classe abstrata não pode ser instanciada
+
+            require_once 'Mamifero.php';
+            require_once 'Reptil.php';
+
+            //Mamifero especialização de Animal
+            $m = new Mamifero();
+            $m->setPeso(26.35);
+            $m->setIdade(1);
+            $m->setMembros(4);
+            $m->setCorPelo("Castanho");
+
+            print_r($m);
+            $m->alimentar();
+            $m->emitirSom();
+            $m->locomever();
+        
+            $r = new Reptil();
+            $r->setPeso(0.956);
+            $r->setIdade(2);
+            $r->setMembros(5);
+            $r->setCorEscama("Acastanhada e verde");
             
-        $m = new Mamifero();
-        $m->setPeso(26.35);
-        $m->setIdade(1);
-        $m->setMembros(4);
-        $m->setCorPelo("Castanho");
-            
-        print_r($m);
-        echo '<br>';
-        $m->alimentar();
-        $m->emitirSom();
-        $m->locomever();
+            print_r($r);
+            $r->alimentar();
+            $r->emitirSom();
+            $r->locomever();
         ?>
         </pre>
     </body>
