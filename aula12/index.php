4523b878b50bfbb947b1ed97c7f25ead8644077c
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-05 12:21:15 +0100

    Aula 12b - Polimorfismo em PHP (Parte 1)

--- aula12/index.php
+++ aula12/index.php
@@ -6,13 +6,23 @@
         <title>Aula 12b - Polimorfismo em PHP (Parte 1)</title>
     </head>
     <body>
-        <?php
+        <pre>       
+ <?php
         // put your code here
             require_once 'Mamifero.php';
             //$a = new Animal(); Uma classe abstrata não pode ser instanciada
             
             $m = new Mamifero();
+            $m->setPeso(26.35);
+            $m->setIdade(1);
+            $m->setMembros(4);
+          
+            print_r($m);
+            echo '<br>';
             $m->alimentar();
+            $m->emitirSom();
+            $m->locomever();
         ?>
+        </pre>
     </body>
 </html>
