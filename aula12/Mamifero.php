f67016feca72242746475b2c9d9fecbb56f24ec6
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-05 12:11:34 +0100

    Aula 12b - Polimorfismo em PHP (Parte 1)

--- aula12/Mamifero.php
+++ aula12/Mamifero.php
@@ -2,5 +2,18 @@
 require_once 'Animal.php';
 
 class Mamifero extends Animal{
-    
+    private $corPelo;
+  
+    public function alimentar() {
+        echo "<p>Em cria. Mamando.</p>";
+    }
+
+    public function emitirSom() {
+        echo "<p>Som de Mamífero! 🦣</p>";
+    }
+
+    public function locomever() {
+        echo '<p>Correndo! </p>';
+    }
+
 }
