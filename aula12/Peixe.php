c032e2ff12cf747e42a7daa3cfd996d023479b5a
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-06 08:44:19 +0100

    Aula 12b - Polimorfismo em PHP (Parte 1)

--- aula12/Peixe.php
+++ aula12/Peixe.php
@@ -5,4 +5,17 @@ class Peixe {
     //Atributos
     private $corDeEscama;
     
+    //Métodos Geter e Seter
+    public function getCorDeEscama() {
+        return $this->corDeEscama;
+    }
+    public function setCorDeEscama($corDeEscama) {
+        $this->corDeEscama = $corDeEscama;
+    }
+
+    //Métodos Públicos
+    public function soltarBolha(){
+        echo "<p>Soltou uma bolha! </p>";
+    }
+
 }
