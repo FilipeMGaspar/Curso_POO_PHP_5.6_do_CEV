a049ac7ef9fac5cfce0882401a50a7339d6a9a7c
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-08 13:41:03 +0100

    Aula 12b - Polimorfismo em PHP (Parte 1)

--- aula12/Ave.php
+++ aula12/Ave.php
@@ -4,4 +4,14 @@ require_once 'Animal.php';
 class Ave {
     //Atributos
     private $corPenas;
+    
+    //Métodos Geter e Seter
+    public function getCorPenas() {
+        return $this->corPenas;
+    }
+    public function setCorPenas($corPenas) {
+        $this->corPenas = $corPenas;
+    }
+
+
 }
