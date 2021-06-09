c4be35ab7fb93439be1d7d1eecc77ad878c1e4ee
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-09 08:58:26 +0100

    Aula 12b - Polimorfismo em PHP (Parte 1)

--- aula12/Cachorro.php
+++ aula12/Cachorro.php
@@ -3,4 +3,8 @@ require_once 'Mamifero.php';
 
 class Cachorro extends Mamifero {
     
+    //Método abstracto sobrescrito
+    public function emitirSom () {
+        echo "Latir! 'ão, ão, ão!'";
+    }
 }
