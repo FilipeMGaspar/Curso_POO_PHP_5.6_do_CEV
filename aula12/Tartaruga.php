79fd4602e39ab91464b13aa8e3422c2c3a045e9e
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-09 12:12:20 +0100

    Aula 12b - Polimorfismo em PHP (Parte 1)

--- aula12/Tartaruga.php
+++ aula12/Tartaruga.php
@@ -2,5 +2,8 @@
 require_once 'Reptil.php';
 
 class Tartaruga extends Reptil {
-    
+    //Método abstrato sobrescrito
+    public function locomover() {
+        echo "<p>Andando beeeem de devagar!  &#X1F422</p>";
+    }
 }
