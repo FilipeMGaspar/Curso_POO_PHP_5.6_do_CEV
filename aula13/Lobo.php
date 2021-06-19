1a072df480f4558ad0c627fdff61193ad3736032
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-19 19:51:54 +0100

    Aula 13b - Polimorfismo Sobrecarga (Parte 2)

--- aula13/Lobo.php
+++ aula13/Lobo.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Lobo
- *
- * @author Aprender
- */
 class Lobo {
-    //put your code here
+    
 }
