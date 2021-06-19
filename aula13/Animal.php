f93e601880e7f44d85dc772db0fbcb68e92e5c8e
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-19 18:57:50 +0100

    Aula 13b - Polimorfismo Sobrecarga (Parte 2)

--- aula13/Animal.php
+++ aula13/Animal.php
@@ -1,16 +1,9 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Animal
- *
- * @author Aprender
- */
-class Animal {
-    //put your code here
+abstract class Animal {     
+    //Atributos
+    protected $peso;
+    protected $idade;
+    protected $membros;
+    
 }
