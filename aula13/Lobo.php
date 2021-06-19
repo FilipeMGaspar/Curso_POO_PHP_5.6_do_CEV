b98979165b8fdf20e8fda6db31d32ab36f0a924a
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-19 20:30:18 +0100

    Aula 13b - Polimorfismo Sobrecarga (Parte 2)

--- aula13/Lobo.php
+++ aula13/Lobo.php
@@ -4,6 +4,6 @@ require_once 'Mamifero.php';
 class Lobo extends Mamifero {
     //Método abstracto sobrescrito
     public function emitirSom() {
-        echo "<p>Auuuuuuuuuuuu! &#X1F43A<p>";
+        echo "<p> &#X1F43A Auuuuuuuuuuuu! &#X1F43A<p>";
     }
 }
