c2da3b0cb9c6677a0d5dc9fca1b7dcb13380e344
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-19 19:11:31 +0100

    Aula 13b - Polimorfismo Sobrecarga (Parte 2)

--- aula13/Mamifero.php
+++ aula13/Mamifero.php
@@ -2,10 +2,21 @@
 require_once 'Animal.php';
 
 class Mamifero extends Animal{
-     //Métodos abstractos sobrescritos
+    //Atributos
+    protected $corPelo;
     
+    //Métodos abstractos sobrescritos    
      public function emitirSom() {
-        
+         echo "<p>Som de Mamídero!</p>";
     }
+
+    //Métodos geter e seter
+    public function getCorPelo() {
+        return $this->corPelo;
+    }
+    public function setCorPelo($corPelo) {
+        $this->corPelo = $corPelo;
+    }
+
 
 }
