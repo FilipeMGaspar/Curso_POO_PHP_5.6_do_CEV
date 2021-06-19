a4299b6e08cf25d1f32c74cec72bb7bfc3554033
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-19 19:15:10 +0100

    Aula 13b - Polimorfismo Sobrecarga (Parte 2)

--- aula13/index.php
+++ aula13/index.php
@@ -6,8 +6,18 @@
         <title>Aula 13b - Polimorfismo Sobrecarga (Parte 2)</title>
     </head>
     <body>
-        <?php
-            //Programa principal
-        ?>
+        <pre>
+            <?php
+                //Programa principal
+                require_once 'Mamifero.php';
+
+                $m = new Mamifero();
+                $m->setPeso(25.6);
+                $m->setIdade(4);
+                $m->setMembros(4);
+                $m->setCorPelo("Catanho");
+                print_r($m);
+            ?>
+        </pre>
     </body>
 </html>
