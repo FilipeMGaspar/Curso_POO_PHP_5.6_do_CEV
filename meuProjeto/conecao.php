c5688a95f9bcc9b10fbcc3e497cd2ce9b67d269b
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-23 20:55:39 +0100

    Exercício de treino 01

---
+++ meuProjeto/conecao.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of conecao
+ *
+ * @author Aprender
+ */
+class conecao {
+    //put your code here
+}
