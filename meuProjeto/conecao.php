8df078aeb477c88c5e8a5e6dcff7b7f9dc9acc0b
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-23 20:57:48 +0100

    Exercício de treino 01

--- meuProjeto/conecao.php
+++ meuProjeto/conecao.php
@@ -6,5 +6,27 @@ class conecao {
     private $dbName;
     private $password;
     
-    
+    //Métodos Geter e Seter
+    public function getUserName() {
+        return $this->userName;
+    }
+    public function setUserName($userName) {
+        $this->userName = $userName;
+    }
+
+    public function getDbName() {
+        return $this->dbName;
+    }
+    public function setDbName($dbName) {
+        $this->dbName = $dbName;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+
 }
