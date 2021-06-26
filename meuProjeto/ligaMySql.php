5f7b013214e4f1f47efaccbfdfc523af0847ac75
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-26 10:14:30 +0100

    Exercício de treino 02

--- meuProjeto/ligaMySql.php
+++ meuProjeto/ligaMySql.php
@@ -2,5 +2,36 @@
 
 class ligaMySql {
     //Atributos
+    private $serverName;
+    private $userName;
+    private $password;
     
+    //Métodos Geter e seter
+    private function getServerName() {
+        return $this->serverName;
+    }
+    private function setServerName($serverName) {
+        $this->serverName = $serverName;
+    }
+
+    private function getUserName() {
+        return $this->userName;
+    }
+    private function setUserName($userName) {
+        $this->userName = $userName;
+    }
+
+    private function getPassword() {
+        return $this->password;
+    }
+    private function setPassword($password) {
+        $this->password = $password;
+    }
+
+    //Criação da ligação á base de dados
+    private function ligarDb() {
+        $this->setServerName("localhost");
+        $this->setUserName("root");
+    }
+
 }
