b542db44d3e3e0f1a601700e035ba7f16f9fd968
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-02 20:19:48 +0100

    Aula 14b - Projeto Final em PHP (Parte 1)

--- aula14/Video.php
+++ aula14/Video.php
@@ -8,5 +8,40 @@ class Video {
     private $curtidas;
     private $repoduzindo;
     
-    
+    //Métodos geter e seter
+    public function getTitulo() {
+        return $this->titulo;
+    }
+    public function setTitulo($titulo) {
+        $this->titulo = $titulo;
+    }
+
+    public function getAvaliacao() {
+        return $this->avaliacao;
+    }
+    public function setAvaliacao($avaliacao) {
+        $this->avaliacao = $avaliacao;
+    }
+
+    public function getViews() {
+        return $this->views;
+    }
+    public function setViews($views) {
+        $this->views = $views;
+    }
+
+    public function getCurtidas() {
+        return $this->curtidas;
+    }
+    public function setCurtidas($curtidas) {
+        $this->curtidas = $curtidas;
+    }
+
+    public function getRepoduzindo() {
+        return $this->repoduzindo;
+    }
+    public function setRepoduzindo($repoduzindo) {
+        $this->repoduzindo = $repoduzindo;
+    }
+
 }
