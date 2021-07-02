abcae5577747650b1ab5d6da6eaffbca64865a82
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-02 20:48:11 +0100

    Aula 14b - Projeto Final em PHP (Parte 1)

--- aula14/Gafanhoto.php
+++ aula14/Gafanhoto.php
@@ -5,8 +5,23 @@ class Gafanhoto {
     private $login;
     private $totoAssistido;
     
+    //Métodos Geter e Seter
+    public function getLogin() {
+        return $this->login;
+    }
+    public function setLogin($login) {
+        $this->login = $login;
+    }
+
+    public function getTotoAssistido() {
+        return $this->totoAssistido;
+    }
+    public function setTotoAssistido($totoAssistido) {
+        $this->totoAssistido = $totoAssistido;
+    }
+
     //Método Público
     public function viuMaisUm() {
-        
+        $this->totoAssistido ++;
     }
 }
