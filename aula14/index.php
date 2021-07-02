9aad2fe9b8e2694424d9b60ba2e206b764239b23
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-02 20:52:49 +0100

    Aula 14b - Projeto Final em PHP (Parte 1)

--- aula14/index.php
+++ aula14/index.php
@@ -13,12 +13,15 @@ and open the template in the editor.
         <pre>
             <?php
                 require_once 'Video.php';
+                require_once 'Gafanhoto.php';
 
                 $v[0] = new Video("Aula 1 de POO");
                 $v[1] = new Video("Aula 12 de PHP");
                 $v[2] = new Video("Aula 15 de HTML5");
 
                 print_r($v);
+                
+                $g = new Gafanhoto("Jubileu", 22, 'M', "Juba");
             ?>
         </pre>
     </body>
