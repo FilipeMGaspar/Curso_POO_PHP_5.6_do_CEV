b2cc85ea1f317161ab48f6056f3b2d15de4c2cce
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-09 08:16:58 +0100

    Aula 15b - Projeto Final em PHP (Parte 2)

--- aula14/Visualizacao.php
+++ aula14/Visualizacao.php
@@ -13,4 +13,20 @@ class Visualizacao {
         $this->filme = $filme;
     }
 
+    //Métodos Geter e Seter
+    public function getEspectador() {
+        return $this->espectador;
+    }
+    public function setEspectador($espectador) {
+        $this->espectador = $espectador;
+    }
+    
+    public function getFilme() {
+        return $this->filme;
+    }
+    public function setFilme($filme) {
+        $this->filme = $filme;
+    }
+
+
 }
